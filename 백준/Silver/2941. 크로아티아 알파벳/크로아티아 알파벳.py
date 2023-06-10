cbd21c567ada07a393024c5cb907f2521fe10261
jsp-KW <70892401+jsp-KW@users.noreply.github.com>
2023-06-10 15:02:50 +0900

    Update 크로아티아 알파벳.py
    쓰지도 않는 변수 count  삭제

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -4,9 +4,7 @@ cro_word = ["c=","c-","dz=", "d-","lj","nj","s=","z="]
 
 string = sys.stdin.readline().rstrip()
 
-count = 0
 for s in cro_word:
     string = string.replace(s,"!")
-
-
+    
 print (len(string))

d9d42346fe097a4e50065bef3dc4d6a8d109cd2f
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2024-02-05 15:54:12 +0900

    [Silver I] Title: 피보나치, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9009. 피보나치/피보나치.py
+++ 백준/Silver/9009. 피보나치/피보나치.py
@@ -2,7 +2,7 @@ import sys
 
 def find(n) : 
         
-    fibo=[1,2] #피보나치를 이용하기 위하여.0,1,1,2,3,5, .... 1134903170
+    fibo=[1,2]
 
     for i in range(1,45):
         fibo.append(fibo[i-1]+ fibo[i])
@@ -11,12 +11,12 @@ def find(n) :
     idx = len(fibo) -1
     sum =0
 
-    while sum !=n: # 주어진 수를 맞춰야하므로
-        while sum + fibo[idx] >n : # 조건에 맞는 값이 제일 뒤에서 최대값이므로
-            idx = idx-1 # 조건을 찾을때까지 뒤에서 찾는다
+    while sum !=n:
+        while sum + fibo[idx] >n :
+            idx = idx-1
         sum += fibo[idx]
         selected.append(fibo[idx])
-    return selected[::-1] # 리스트를 뒤집어서 반환한다. 오름차순으로 출력하기 위해서
+    return selected[::-1]
 
 
 n = int (sys.stdin.readline().rstrip('\n'))
@@ -28,4 +28,4 @@ for _ in range(n):
 
 for i in input:
     result = find(i)
-    print(*result)# *을 이용해서 값1 값2 값3 의 형태로 출력한다.
+    print(*result)

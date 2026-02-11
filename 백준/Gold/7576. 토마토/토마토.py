f1e3318f9aa301a4596a4914107f8637cabf760d
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-02-11 16:20:39 +0900

    [Gold V] Title: 토마토, Time: 300 ms, Memory: 173692 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,44 +1,53 @@
 import sys
 from collections import deque
 
-M, N = map(int,sys.stdin.readline().split())
-# 1 :  익은 토마토, 0 : 익지 않은 토마토 , -1: 토마토 X
 
-tomato_box = [[0]*(M) for _ in range (N)]
+M,N = map(int,sys.stdin.readline().split())
+
+board = [[] for _ in range (N)] 
 
 for i in range (N) :
-    temp = list (map(int, sys.stdin.readline().split()))
-    tomato_box[i] = temp
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+    temp = list(map(int,sys.stdin.readline().split()))
+    board[i].extend(temp)
 
 
-start_nodes = deque()
-day_count = [[-1]*(M) for _ in range (N)]
+start_cordinates = []
+visited = [[-1]*(M) for _ in range (N)]
 
 for i in range (N) :
     for j in range (M) :
-        if tomato_box[i][j] ==1 :
-            start_nodes.append ((i,j))
-            day_count[i][j] = 0
+        if board[i][j] == 1 :
+            start_cordinates.append((i,j)) # 시작점 넣기
+            visited[i][j] = 0
 
 
-while start_nodes :
+dy = [-1,1,0,0]
+dx= [0,0,-1,1]
+def bfs () :
+    while q  :
+        cur_y,cur_x = q.popleft()
+        for i in range (4) :
+            my,mx = cur_y + dy[i] , cur_x +dx[i]
+            if 0<=my<N  and 0<= mx <M :
+                if board[my][mx] == 0 and visited[my][mx] == -1:
+                    visited[my][mx] = visited[cur_y][cur_x] +1
+                    board[my][mx] = 1
+                    q.append((my,mx))
+
+
+q = deque(start_cordinates)
+
+bfs()
+
+day = 0
+for i in range (N) :
+    for j in range (M) :
+        if board[i][j] == 0 :
+            print(-1)
+            sys.exit()
+        if visited[i][j] > day :
+            day = visited[i][j]
     
-    y,x = start_nodes.popleft()
 
-    for i in range (4) :
-        new_y, new_x = y+dy[i] , x+dx[i]
-        if 0<= new_x <M  and 0<= new_y <N:
-            if tomato_box[new_y][new_x] == 0  and day_count[new_y][new_x] ==-1 : # 익지 않았는 경우에만
-                tomato_box[new_y][new_x] = 1 # 익음 처리
-                day_count [new_y][new_x] = day_count[y][x] +1 # 익는거 하루 +1
-                start_nodes.append ((new_y, new_x))
-
-for i in range(N) :
-    for j in range (M ):
-        if tomato_box[i][j] == 0:
-            print (-1)
-            exit()
-            
-print (max(max(row) for row in day_count))
+print (day if day != 0 else 0)
+        

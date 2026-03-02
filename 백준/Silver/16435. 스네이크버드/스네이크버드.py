ae6d38443b33d5a8a062d0aceed7031fbd8e4e4e
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-03-02 16:53:22 +0900

    [Silver V] Title: 스네이크버드, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.py
@@ -0,0 +1,19 @@
+import sys
+
+# 과일 -> 길이 +1
+# i번째 과일 높이 hi
+# 자신의 길이보다 작거나,같은 높이 과일 먹을 수 있음
+# L ->  과일들을 먹어서 성장할 수 있는 최대 길이
+
+
+N, L = map(int,sys.stdin.readline().split())
+
+heights = list(map(int,sys.stdin.readline().split()))
+
+heights.sort()
+
+for h in heights :
+    if h <= L  :
+        L= L+1 
+
+print(L)

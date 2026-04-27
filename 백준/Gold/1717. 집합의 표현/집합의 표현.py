f0fa6f433d7fc36e7d6cf757665dea5f6a04a2e5
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-04-27 14:56:17 +0900

    [Gold V] Title: 집합의 표현, Time: 212 ms, Memory: 121668 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,33 +1,51 @@
-#union find 트리 문제
 import sys
-N,M = map(int,sys.stdin.readline().split()) 
-parent =  [i for i in range(N+1)] # 배열 초기화
 
-def find(x) : # 밑과 같은 재귀호출로 짤 경우 메모리 초과 이슈 터짐
-    # if parent[x] != x: # 위에 연결된 부모 노드가 존재하면
-    #     parent[x] =  find(parent[x]) # 6 4 3 2 면 바로 2로 압축시키는 
-    # return parent[x]
+n,m = map(int,sys.stdin.readline().split())
 
-    # 비재귀적 방식으로 변환해서 풀어야함
-    while parent[x] != x:
+parent = [i for i in range (n+1)]
+
+answers= []
+
+
+def find(parent, x) :
+
+
+    #x의 부모의 값을 할아버지 값으로 대체해서 올라간다
+    while parent[x] != x :
         parent[x] = parent[parent[x]]
         x = parent[x]
     return x
 
-# 큰 값의 부모가 작은 값이 되도록 
-def union(a,b):
-    get_a = find(a)
-    get_b = find(b)
 
-    if get_a < get_b :
-        parent[get_b] = get_a
+
+def union (parent, A,B) :
+    rootA = find(parent,A)
+    rootB = find(parent,B)
+
+    if rootA < rootB :
+        parent[rootB] = rootA
+    elif rootA == rootB :
+        return False
+    
     else :
-        parent[get_a] = get_b
+        parent[rootA] = rootB
+
+    return True
 
 
-for _ in range(M): # _식으로 사용하는것도 익숙해지기
-    operation, a,b =  map(int, sys.stdin.readline().split())
-    if operation ==0 :
-        union(a,b)
-    else :
-        print("YES" if find(a) == find(b) else "NO")
+for _ in range (m) :
+    op,a,b = map(int,sys.stdin.readline().split())
+    if op == 0 :
+        union (parent, a,b) 
+    elif op ==1 :
+        if find(parent,a) == find (parent,b) :
+            answers.append("YES")
+        else:
+            answers.append("NO")
+
+    
+for ans in answers :
+    print(ans)
+
+
+

ba90e9681981232293ff863da1861bd048495982
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-05-08 19:22:23 +0900

    [level 1] Title: 폰켓몬, Time: 2.78 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer =0;
+        int get = nums.length /2 ;
+        
+        HashSet <Integer> set = new HashSet <>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        answer = Math.min(set.size(), get);
+        
+	    return answer;
+    }
+}

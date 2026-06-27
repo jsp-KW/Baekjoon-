e8ec0bee695ed6ffe8bb003509df7b8551229b5d
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-06-27 22:05:12 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -1,5 +1,34 @@
--- 코드를 입력하세요
-SELECT ORDER_ID, PRODUCT_ID , DATE_FORMAT(OUT_DATE,'%Y-%m-%d') AS 'OUT_DATE' , CASE WHEN OUT_DATE IS NULL THEN '출고미정'
-WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
-ELSE '출고대기' END AS '출고여부' FROM FOOD_ORDER
+# -- 코드를 입력하세요
+# SELECT ORDER_ID, PRODUCT_ID , DATE_FORMAT(OUT_DATE,'%Y-%m-%d') AS 'OUT_DATE' , CASE WHEN OUT_DATE IS NULL THEN '출고미정'
+# WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
+# ELSE '출고대기' END AS '출고여부' FROM FOOD_ORDER
+# ORDER BY ORDER_ID ASC;
+
+
+
+
+
+
+
+
+-- 5월 1일까지는 출고완료
+-- 5월 2일부터는 OUT_DATE가 출고대기
+-- 널값이라면 출고미정
+
+
+SELECT ORDER_ID, PRODUCT_ID, OUT_DATE, CASE 
+WHEN OUT_DATE IS NULL THEN '출고미정'
+WHEN OUT_DATE >= '2022-05-02' THEN '출고대기'
+ELSE '출고완료' END AS '출고여부' FROM FOOD_ORDER 
 ORDER BY ORDER_ID ASC;
+
+
+
+
+
+
+
+
+
+
+

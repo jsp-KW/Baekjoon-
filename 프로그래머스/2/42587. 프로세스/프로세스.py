76af243da1ccae5afea8a7c4840cfa17c8c30477
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-02 17:43:42 +0900

    [level 2] Title: 프로세스, Time: 0.23 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,38 @@
+
+def solution(priorities, location):
+    
+    # 실행 대기큐의 프로세스 중요도가 담긴 배열
+    # 몇번째로 실행되는지 알고싶은 프로세스의 위치 location
+    # 
+    
+    answer=0
+    queue = [(index, weight) for index,weight in enumerate(priorities)]
+   
+    while True :
+        temp= queue.pop(0)
+        if any(temp[1]< q[1] for q in queue) :
+            queue.append(temp)
+        else:
+            answer+=1
+            if temp[0] == location :
+                break
+                
+    return answer
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

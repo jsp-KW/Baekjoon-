2410d67d17627ff5dc77090fe631f19552845d33
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-02 19:07:40 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -2,36 +2,78 @@ import heapq
 # 최소비용정리,
 
 def solution(n, costs):
-    # n : 섬의 개수 
-    graph = [[] for _ in range(n) ]
-    for cost in costs :
-        island1, island2, c = cost 
-        graph [island1].append ((c,island2))
-        graph [island2].append ((c,island1))
+#     # n : 섬의 개수 
+#     graph = [[] for _ in range(n) ]
+#     for cost in costs :
+#         island1, island2, c = cost 
+#         graph [island1].append ((c,island2))
+#         graph [island2].append ((c,island1))
     
     
-    # 다익스트라
-    # 모든섬 통행 -> 최소비용
-    def prim (start) :
-        visited = [False] *(n)
-        cnt = 0
-        total_cost = 0
-        q = [(0,start)]
+#     # 다익스트라
+#     # 모든섬 통행 -> 최소비용
+#     def prim (start) :
+#         visited = [False] *(n)
+#         cnt = 0
+#         total_cost = 0
+#         q = [(0,start)]
         
-        while q :
-            cur_cost, cur_node = heapq.heappop(q) 
-            if visited[cur_node]  : #방문한 곳이라면? 스킵
-                continue
-            visited[cur_node] = True
-            total_cost = total_cost + cur_cost
-            cnt +=1
-            if cnt == n :
-                return total_cost
-            for nxt_cost, nxt_node in graph [cur_node] :
-                if not visited[nxt_node] :
-                    heapq.heappush (q,(nxt_cost,nxt_node))
+#         while q :
+#             cur_cost, cur_node = heapq.heappop(q) 
+#             if visited[cur_node]  : #방문한 곳이라면? 스킵
+#                 continue
+#             visited[cur_node] = True
+#             total_cost = total_cost + cur_cost
+#             cnt +=1
+#             if cnt == n :
+#                 return total_cost
+#             for nxt_cost, nxt_node in graph [cur_node] :
+#                 if not visited[nxt_node] :
+#                     heapq.heappush (q,(nxt_cost,nxt_node))
             
             
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    graph = [[] for _ in range (n)]
+    
+    for c in costs:
+        start,end,cost= c
+        graph[start].append((cost,end))
+        graph[end].append((cost,start))
+        
+    visited = [False]*(n)
+    
+    
+    def prim(start):
+        
+        cnt=0
+        total_cost =0
+        q= [(0, start)]
+
+        while q:
+            cur_cost, cur_node = heapq.heappop(q)
+        
+            if visited[cur_node] :
+                continue
+            
+            total_cost += cur_cost
+            visited[cur_node]= True
+            cnt+=1
+            if cnt == n :
+                return total_cost 
+            
+
+            for nxt_cost, nxt_node in graph[cur_node]:
+                if not visited[nxt_node]:
+                    heapq.heappush(q, (nxt_cost,nxt_node))
+
         
     return prim(0)

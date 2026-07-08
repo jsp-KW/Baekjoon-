f481b5b007389e4cef1ffe828cd584c1a962fa67
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-08 22:19:22 +0900

    [level 3] Title: 양과 늑대, Time: 1.96 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -0,0 +1,42 @@
+def solution(info, edges):
+    answer = 0
+    
+    n = len(info)
+    Tree = [[] for _ in range (n)]
+    
+    for e in edges:
+        start,end = e
+        Tree[start].append(end)
+    
+    
+    answer =0
+    
+    def dfs(sheep,wolf, candidates) :
+        nonlocal answer
+        
+        answer = max(answer, sheep)
+        
+        for node in candidates:
+            next_sheep = sheep
+            next_wolf = wolf
+            
+            if info[node] ==0 :
+                next_sheep +=1
+            
+            else :
+                next_wolf +=1
+            
+            if next_wolf >= next_sheep :
+                continue
+                
+            next_candidates = candidates.copy()
+            next_candidates.remove(node)
+            next_candidates.extend(Tree[node])
+            
+            dfs(next_sheep, next_wolf, next_candidates)
+    
+    dfs(1,0,Tree[0])
+        
+        
+        
+    return answer

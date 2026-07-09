c7d2cb28fcc4e38d5d35f504db1ce5d2369e71ad
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-09 21:05:52 +0900

    [level 3] Title: 양과 늑대, Time: 3.34 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 1.96 ms
+메모리: 11.5 MB, 시간: 3.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 08일 22:19:18
+2026년 07월 09일 21:05:49
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -2,41 +2,102 @@ def solution(info, edges):
     answer = 0
     
     n = len(info)
-    Tree = [[] for _ in range (n)]
     
-    for e in edges:
+    tree = [[]*(n) for _ in range (n)]
+    for e in edges :
         start,end = e
-        Tree[start].append(end)
-    
-    
-    answer =0
-    
-    def dfs(sheep,wolf, candidates) :
+        tree[start].append(end)
+
+    def dfs(sheep, wolf,candidates):
         nonlocal answer
+        answer= max(answer, sheep)
+
+        for nxt in candidates:
+            next_sheep=sheep
+            next_wolf =wolf
         
-        answer = max(answer, sheep)
-        
-        for node in candidates:
-            next_sheep = sheep
-            next_wolf = wolf
-            
-            if info[node] ==0 :
+            if info[nxt] == 0 :
                 next_sheep +=1
             
-            else :
+            if info[nxt] ==1 :
                 next_wolf +=1
-            
-            if next_wolf >= next_sheep :
+                    
+            if next_sheep <= next_wolf :
                 continue
                 
             next_candidates = candidates.copy()
-            next_candidates.remove(node)
-            next_candidates.extend(Tree[node])
-            
+            next_candidates.remove(nxt)
+            next_candidates.extend(tree[nxt])
             dfs(next_sheep, next_wolf, next_candidates)
     
-    dfs(1,0,Tree[0])
+    dfs(1, 0, tree[0])
+    
+    return answer
+            
+            
+            
         
         
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
         
     return answer
+
+#  n = len(info)
+#     Tree = [[] for _ in range (n)]
+    
+#     for e in edges:
+#         start,end = e
+#         Tree[start].append(end)
+    
+    
+#     answer =0
+    
+#     def dfs(sheep,wolf, candidates) :
+#         nonlocal answer
+        
+#         answer = max(answer, sheep)
+        
+#         for node in candidates:
+#             next_sheep = sheep
+#             next_wolf = wolf
+            
+#             if info[node] ==0 :
+#                 next_sheep +=1
+            
+#             else :
+#                 next_wolf +=1
+            
+#             if next_wolf >= next_sheep :
+#                 continue
+                
+#             next_candidates = candidates.copy()
+#             next_candidates.remove(node)
+#             next_candidates.extend(Tree[node])
+            
+#             dfs(next_sheep, next_wolf, next_candidates)
+    
+#     dfs(1,0,Tree[0])
+        
+        

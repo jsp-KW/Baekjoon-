89ae94441c5a1bdcff523caf0aaa5752f8a1c9f3
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-18 17:59:19 +0900

    [level 2] Title: 더 맵게, Time: 659.97 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,31 +3,25 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         
-        PriorityQueue <Integer> pq = new PriorityQueue <>();
-        
-        for (int i=0; i<scoville.length; i++) {
-            pq.offer (scoville[i]);
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
+        for (int n : scoville) {
+            pq.offer(n);
+        }
+        while ((pq.size() >=2) && (pq.peek() <K)) {
+            int first = pq.poll();
+            int second = pq.poll();
+            int temp = first + (2*second);
+            pq.offer(temp);
+            answer+=1;
         }
         
-        // offer 넣기 / poll 제일 작은 값 나옴
-        
-        //  최소힙의 루트가 k보다 작을때까지
-        // 두개 배서 연산하고 결과값이 k보다 작을때까지?
-        
-        while  ((pq.size () >=2 ) && (pq.peek () < K)) {
-            int num1 = pq.poll();
-            int num2 = pq.poll();
-            
-            int scovill_res = num1  + (num2 *2);
-            pq.offer (scovill_res);
-            answer +=1;
+        if (pq.peek() >= K) {
+            return answer;
         }
+        return -1;
+       
         
-        // 더이상 섞지 못하는데 k보다 작은 경우
-        
-        if (pq.peek() <K)
-            return  -1;
-        
-        return answer;
+    
+  
     }
 }

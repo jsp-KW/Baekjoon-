d08149e28313e1877bf5e9085d7c411d0a844fff
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-19 14:44:56 +0900

    [level 2] Title: 가장 큰 수, Time: 104.99 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,35 +2,54 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        // 정수를 이어붙여 만들수 있는 가장 큰 수
+//         String answer = "";
+//         // 정수를 이어붙여 만들수 있는 가장 큰 수
         
-        String [] nums = new String [numbers.length];
+//         String [] nums = new String [numbers.length];
         
-        for (int i =0; i< numbers.length; i++) {
-            nums[i] = String.valueOf(numbers[i]);
-        }
+//         for (int i =0; i< numbers.length; i++) {
+//             nums[i] = String.valueOf(numbers[i]);
+//         }
         
-        //  compare (a,b) -> 값 양수인 경우  b a 순
-        //  음수인 경우 a b 순
-        //  0 이면 그대로
-        
-        
-        Arrays.sort (nums, (a,b)-> (b+a).compareTo(a+b)); // 610 > 106 -> 양수 리턴  a= 10 , b =6  ->즉  b a로 바뀜
-        
-        if (nums[0].equals("0")) {
-            return "0";
-        }
+//         //  compare (a,b) -> 값 양수인 경우  b a 순
+//         //  음수인 경우 a b 순
+//         //  0 이면 그대로
         
         
-        StringBuilder sb = new StringBuilder ();
+//         Arrays.sort (nums, (a,b)-> (b+a).compareTo(a+b)); // 610 > 106 -> 양수 리턴  a= 10 , b =6  ->즉  b a로 바뀜
         
-        for (String s : nums) {
-            sb.append(s);
-        }
-        answer = sb.toString();
-        return answer;
-    
+//         if (nums[0].equals("0")) {
+//             return "0";
+//         }
+        
+        
+//         StringBuilder sb = new StringBuilder ();
+        
+//         for (String s : nums) {
+//             sb.append(s);
+//         }
+//         answer = sb.toString();
+//         return answer;
+        
+      String answer = "";
+      String [] nums = new String[numbers.length];
+      for (int i =0; i< numbers.length; i++) {
+          nums[i] = String.valueOf(numbers[i]);
+      }  
+        
+      Arrays.sort (nums, (a,b)-> (b+a).compareTo(a+b));
+      
+      if (nums[0].equals("0")){
+          return "0";
+      }
+        
+      StringBuilder sb = new StringBuilder();
+      for (String s : nums) {
+          sb.append(s);
+      }
+        
+    answer = sb.toString();
+    return answer;
     }
     
 }

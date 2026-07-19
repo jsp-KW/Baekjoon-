477361d057072ccbd3070f9a7c450d4bc5c947a6
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-19 19:21:37 +0900

    [level 2] Title: 배달, Time: 0.62 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=python3) 
 
 ### 성능 요약
 
-메모리: 75.3 MB, 시간: 4.06 ms
+메모리: 12.1 MB, 시간: 0.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 19일 19:14:06
+2026년 07월 19일 19:21:34
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,44 +1,73 @@
 import heapq 
 
 def solution(N, road, K):
-    answer = 0
-    graph = [[] for _ in range (N+1) ]
+#     answer = 0
+#     graph = [[] for _ in range (N+1) ]
+#     for r in road :
+#         a= r[0]
+#         b= r[1]
+#         cost = r[2]
+#         graph[a].append((cost, b))
+#         graph[b].append((cost,a))
+    
+#     INF = float('inf')
+#     distance = [INF] *(N+1) 
+        
+#     def dijkstra(start) :
+#         q = [(0,start)]
+   
+#         distance[start] = 0
+        
+#         while q  :
+#             cur_cost, cur_node = heapq.heappop(q) 
+            
+#             if cur_cost > distance[cur_node] :
+#                 continue 
+            
+#             for nxt_cost, nxt_node in graph[cur_node] :
+#                 total_cost = nxt_cost + cur_cost
+#                 if total_cost < distance[nxt_node] :
+#                     distance [nxt_node] = total_cost 
+#                     heapq.heappush (q,(total_cost, nxt_node))
+    
+        
+#     dijkstra(1)
+#     for i in range (1, N+1) :
+#         if distance[i] <= K :
+#             answer +=1
+
+    answer =0
+    graph = [[] for _ in range (N+1)]
+    
     for r in road :
-        a= r[0]
+        a = r[0]
         b= r[1]
         cost = r[2]
         graph[a].append((cost, b))
         graph[b].append((cost,a))
     
     INF = float('inf')
-
-    def dijkstra(start,end) :
-        q = [(0,start)]
-            
-        distance = [INF] *(N+1) 
+    distance = [INF] * (N+1)
+    distance[1] = 0
     
-        
-        distance[start] = 0
-        
-        while q  :
-            cur_cost, cur_node = heapq.heappop(q) 
-            
-            if cur_cost > distance[cur_node] :
-                continue 
-                
-            if cur_node == end :
-                return distance[end]
-            
-            for nxt_cost, nxt_node in graph[cur_node] :
-                total_cost = nxt_cost + distance[cur_node]
-                if total_cost < distance[nxt_node] :
-                    distance [nxt_node] = total_cost 
-                    heapq.heappush (q,(total_cost, nxt_node))
+    pq = [(0,1)]
     
-    for i in range (1, N+1) :
-        result = dijkstra(1,i)
-        if result <= K :
-            answer +=1
-
+    while pq :
         
+        cur_cost, cur_node= heapq.heappop(pq)
+        if (distance[cur_node] < cur_cost) :
+            continue
+            
+        distance[cur_node] = cur_cost
+        
+        for nxt_cost, nxt_node in graph[cur_node]:
+            total_cost = cur_cost + nxt_cost
+            if distance[nxt_node] > total_cost :
+                distance[nxt_node] = total_cost
+                heapq.heappush(pq, (total_cost, nxt_node))
+    
+    
+    for num in distance :
+        if num <= K :
+            answer+=1
     return answer

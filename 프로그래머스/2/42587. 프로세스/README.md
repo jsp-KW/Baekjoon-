2b6a91737c623a60cf4ead0520933eec4b77a1b7
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-19 21:03:09 +0900

    [level 2] Title: 프로세스, Time: 1.33 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 0.27 ms
+메모리: 89.6 MB, 시간: 1.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 19일 19:26:38
+2026년 07월 19일 21:03:04
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -4,44 +4,79 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        // java로 mapping
+//         // java로 mapping
         
-        ArrayDeque <int[]> q = new ArrayDeque<>();
-        int idx=0;
-        for (int item : priorities) {
-            int weight = item;
-            q.offer (new int []{idx, weight});
-            idx+=1;
+//         ArrayDeque <int[]> q = new ArrayDeque<>();
+//         int idx=0;
+//         for (int item : priorities) {
+//             int weight = item;
+//             q.offer (new int []{idx, weight});
+//             idx+=1;
+//         }
+        
+        
+//         while (true) {
+//             int [] target = q.poll();
+//             int target_idx = target[0];
+//             int target_weight = target[1];
+            
+//             boolean check = true;
+//             for (int [] arr:  q) {
+                
+//                 int ch_index = arr[0];
+//                 int ch_weight = arr[1];
+//                 if (target_weight < ch_weight) {
+//                     check = false;
+//                     break;
+//                 }
+//             }
+            
+//             if (!check) {
+//                 q.offer(new int[]{target_idx, target_weight});
+//             }
+//             else {
+//                 answer+=1;
+//                 if (target_idx == location) {
+//                     break;
+//                 }
+//             }
+            
+//         }
+        
+        ArrayDeque <int []> q = new ArrayDeque<>();
+        
+        for (int i=0; i< priorities.length; i++) {
+            q.offer(new int[] {i, priorities[i]});
         }
         
-        
         while (true) {
-            int [] target = q.poll();
-            int target_idx = target[0];
-            int target_weight = target[1];
+            int [] cur = q.poll();
+            int cur_idx = cur[0];
+            int cur_weight =cur[1];
             
             boolean check = true;
-            for (int [] arr:  q) {
+            
+            for(int [] temp : q) {
+                int temp_weight = temp[1];
                 
-                int ch_index = arr[0];
-                int ch_weight = arr[1];
-                if (target_weight < ch_weight) {
+                if (temp_weight > cur_weight) {
                     check = false;
                     break;
                 }
             }
             
-            if (!check) {
-                q.offer(new int[]{target_idx, target_weight});
+            if(!check)  {
+                q.offer(new int [] {cur_idx, cur_weight});
             }
-            else {
+            else{
                 answer+=1;
-                if (target_idx == location) {
+                if (location == cur_idx) {
                     break;
                 }
             }
             
         }
+        
         return answer;
     }
 }

143d3c9e884b175696f06810b1ec4cbcc700036c
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-07-25 00:16:45 +0900

    [level 2] Title: 구명보트, Time: 7.76 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,27 +1,51 @@
 def solution(people, limit):
-    answer = 0
-    left, right = 0, len(people)-1
+#     answer = 0
+#     left, right = 0, len(people)-1
+    
+#     people.sort()
+    
+#     # 모든 사람들을 구출하기 위해 필요한 구명보트 개수의 최소값
+    
+#     # 50 50 70 80
+    
+#     while left <= right :
+#         cur_weight= 0
+#         cur_weight= people[left] + people[right]
+        
+#         if cur_weight <= limit :
+#             left = left +1
+#             right= right -1
+        
+#         else :
+#             right = right -1
+        
+#         answer +=1
+        
+        
+#     return answer
+    
+    
+    
+    
+    
+    answer =0
     
     people.sort()
+    left = 0
+    right = len(people)-1
     
-    # 모든 사람들을 구출하기 위해 필요한 구명보트 개수의 최소값
+    while left <=right :
+        temp = people[left] + people[right]
+        
+        if temp <=limit :
+            left+=1
+            right -=1
+        else:
+            right -=1
+        answer+=1
+        
     
-    # 50 50 70 80
     
-    while left <= right :
-        cur_weight= 0
-        cur_weight= people[left] + people[right]
-        
-        if cur_weight <= limit :
-            left = left +1
-            right= right -1
-        
-        else :
-            right = right -1
-        
-        answer +=1
-        
-        
     return answer
     
     
@@ -33,24 +57,3 @@ def solution(people, limit):
     
     
     
-    
-    
-    
-    
-    
-#     left,right = 0,len(people) -1 
-    
-#     #구멍 보트를 최대한 적개 사용하자
-#     people.sort ()
-#     # 50 50 70 80
-#     # 80 쓰고, 70쓰고, 50+50
-
-#     while left <= right :
-#         if people[left] + people[right] <= limit :
-#             left += 1
-#             right-=1
-#         else :
-#             right -=1 
-#         answer+=1
-    
-#     return answer

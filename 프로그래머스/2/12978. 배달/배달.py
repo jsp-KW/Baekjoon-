b4ad430587de7b75970114dd15c85dc0fb1a6e34
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-08-01 10:38:27 +0900

    [level 2] Title: 배달, Time: 0.58 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -36,38 +36,63 @@ def solution(N, road, K):
 #         if distance[i] <= K :
 #             answer +=1
 
+  
+    
+    graph = [[] *(N+1) for _ in range (N+1)]
     answer =0
-    graph = [[] for _ in range (N+1)]
+    # k시간 이하로 배달 
+    
+    #1번마을에서
     
     for r in road :
-        a = r[0]
-        b= r[1]
+        start = r[0]
+        end = r[1]
         cost = r[2]
-        graph[a].append((cost, b))
-        graph[b].append((cost,a))
+        
+        graph[start].append((cost, end))
+        graph[end].append((cost, start))
+        
     
     INF = float('inf')
-    distance = [INF] * (N+1)
-    distance[1] = 0
-    
-    pq = [(0,1)]
-    
-    while pq :
+    distance = [INF]*(N+1)
+   
+    def dijkstra(start) :
+
+        q=[(0,start)]
+        distance[start] = 0
         
-        cur_cost, cur_node= heapq.heappop(pq)
-        if (distance[cur_node] < cur_cost) :
-            continue
+        
+        while q  :
+            cur_cost, cur_node = heapq.heappop(q)
             
-        distance[cur_node] = cur_cost
-        
-        for nxt_cost, nxt_node in graph[cur_node]:
-            total_cost = cur_cost + nxt_cost
-            if distance[nxt_node] > total_cost :
-                distance[nxt_node] = total_cost
-                heapq.heappush(pq, (total_cost, nxt_node))
+            if distance[cur_node] < cur_cost :
+                continue
+            
+            for nxt_cost, nxt_node in graph[cur_node] :
+                total_cost = distance[cur_node] +nxt_cost 
+                if total_cost < distance[nxt_node] :
+                    distance[nxt_node] = total_cost
+                    heapq.heappush(q,(total_cost, nxt_node))
     
+    dijkstra(1)
     
-    for num in distance :
-        if num <= K :
+    for i in range (1, N+1) :
+        if distance[i] <= K :
             answer+=1
+        
+    
     return answer
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

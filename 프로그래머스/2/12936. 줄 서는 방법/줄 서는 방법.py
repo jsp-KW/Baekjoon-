f2e9e6608bffe49ac834b47e6172e2803b042a43
essay_jung <70892401+jsp-KW@users.noreply.github.com>
2026-08-09 13:03:18 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,28 +1,20 @@
-from itertools import permutations 
-
 def solution(n, k):
     answer = []
     
-    # n-1 개
-    # 1개당, 그개 n개 * (n-1)!
-    
-    def fact (n) :
-        if n <=1 :
+    nums = [i for i in range (1,n+1)]
+    # nums[idx] 로 숫자 넣음
+    def get_fact (n) :
+        if n <=1:
             return 1
         else :
-            return n* fact(n-1)
+            return n*get_fact(n-1)
         
-    k = k-1
-    set_count = k // fact (n-1)
-    
-    
-    nums = [i for i in range (1,n+1)]
-    cnt = 1
-    
-    while n>0 :
-        block = fact(n-1)
-        idx= k // block
-        answer.append(nums.pop(idx))
-        k= k% block
+    k = k-1 
+    while n > 0 :
+        block = get_fact (n-1)
+        idx =  k // block
+        answer.append (nums.pop(idx))
+        k = k % block # 자리 몇번째인지
         n-=1
+    
     return answer
